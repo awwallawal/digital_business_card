4168c5317849a5872fcebeec62bdf154e892d2b0
Awwal <awwallawal@gmail.com>
2023-05-03 01:31:32 +0100

    Change the image files to use the import method to call the image files

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { Twitter } from "../src/icons8-twitter-50.png";
+import { Facebook } from "../src/icons8-facebook-f-50.png";
+import { Instagram } from "../src/icons8-instagram-26.png";
+import { GitHub } from "../src/icons8-github-64.png";
+
 
 function Footer() {
  return (
    <div className="Footer">
       <div className="box-icon" >
-          <a href="https://twitter.com/hawwal" target="_blank"><img className="box-icon-image" src="/src/icons8-twitter-50.png" alt="" /></a>
+          <a href="https://twitter.com/hawwal" target="_blank"><img className="box-icon-image" src={Twitter} alt="" /></a>
       </div>
       <div className="box-icon">
-          <a href="https://www.facebook.com/awwallawal?mibextid=ZbWKwL" target="_blank"><img className="box-icon-image" src="/src/icons8-facebook-f-50.png" alt="" /></a>
+          <a href="https://www.facebook.com/awwallawal?mibextid=ZbWKwL" target="_blank"><img className="box-icon-image" src={Facebook} alt="" /></a>
       </div>
       <div className="box-icon">
-          <a href="https://www.instagram.com/hawwallawal/" target="_blank"><img className="box-icon-image" src="/src/icons8-instagram-26.png" alt="" /></a>
+          <a href="https://www.instagram.com/hawwallawal/" target="_blank"><img className="box-icon-image" src={Instagram} alt="" /></a>
       </div>
       <div className="box-icon">
-          <a href="https://github.com/awwallawal" target="_blank"><img className="box-icon-image" src="/src/icons8-github-64.png" alt="" /></a>
+          <a href="https://github.com/awwallawal" target="_blank"><img className="box-icon-image" src={GitHub} alt="" /></a>
       </div>
    </div>
  )
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Picture } from "../src/Awwal.jpg"
 
 function Header() {
  return (
    <>
     <div className="Header">
-      <img src="/src/Awwal.jpg" alt="a passport photograph" className="Header-img"/>
+      <img src={Picture} alt="a passport photograph" className="Header-img"/>
     </div>
    </>
  )
