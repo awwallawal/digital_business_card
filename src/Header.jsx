ea8ed5bc4f9404582dbac29494ea67a2d0a71c23
Awwal <awwallawal@gmail.com>
2023-05-03 08:17:36 +0100

    Changed the images location by changing its directory and putting all images in an images folder

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Picture } from "../src/Awwal.jpg"
 
 function Header() {
  return (
    <>
     <div className="Header">
-      <img src={Picture} alt="a passport photograph" className="Header-img"/>
+      <img src="/src/images/Awwal.jpg" alt="a passport photograph" className="Header-img"/>
     </div>
    </>
  )
